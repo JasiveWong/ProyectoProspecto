5a9c2849618ecb2cc2decdcfa4f7f2573df3f7f3
JasiveWong <jasivemw@gmail.com>
2022-06-21 12:23:16 -0600

    boton salir en captura de prospectos

--- js/evaluacion.js
+++ js/evaluacion.js
@@ -6,3 +6,9 @@ function mostrarTextArea(){
         resultado.classList.remove("mostrar");
     }
 }
+function Salir(){
+    let salir=confirm("Si sale perdera toda la captura ¿Seguro que quiere salir?");
+    if(salir){
+        location.href="listadoProspectos.php";
+    }
+}
