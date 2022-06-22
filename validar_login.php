eef87fc9ceaa158b5e63f8f3cf611f163547d586
JasiveWong <jasivemw@gmail.com>
2022-06-22 16:04:50 -0600

    Evita validar login sin iniciar sesion

--- validar_login.php
+++ validar_login.php
@@ -1,40 +1,44 @@
 <?php
+    if(isset($_SESSION['usuario'])&& isset($_SESSION['trabajador'])){
+        $usuario = $_POST['usuario'];
+        $captcha = $_POST['g-recaptcha-response'];
+        $contra = $_POST['contra'];
 
-    $usuario = $_POST['usuario'];
-    $captcha = $_POST['g-recaptcha-response'];
-
-    if(!empty($captcha)){
-        $secret = "6LdOVQMgAAAAAE1DUZi6vOAeA7YFMzfQw5Y7gIam";
-        $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha");
-        $arr=json_decode($response,TRUE);
-        if($arr["success"]){
-            session_start();
-            include("bd.php");
-            $conexionbd=conectarbd();
-            $consulta = "SELECT*FROM usuarios WHERE usuario= '$usuario'";
-            $resultado = mysqli_query($conexionbd,$consulta);
-            $fila = mysqli_fetch_assoc($resultado);
-            if(password_verify($contra, $fila['contrasenia'])){
-                SESSION_START();
-                $_SESSION['usuario']=$usuario;
-                $_SESSION['trabajador']=$fila['tipo'];
-                if($fila['tipo'] == "Promotor"){
-                    header("location:listadoProspectos.php");
-                }else{
-                    header("location:listaProspectosEvaluar.php");
+        if(!empty($captcha)){
+            $secret = "6LdOVQMgAAAAAE1DUZi6vOAeA7YFMzfQw5Y7gIam";
+            $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha");
+            $arr=json_decode($response,TRUE);
+            if($arr["success"]){
+                session_start();
+                include("bd.php");
+                $conexionbd=conectarbd();
+                $consulta = "SELECT*FROM usuarios WHERE usuario= '$usuario'";
+                $resultado = mysqli_query($conexionbd,$consulta);
+                $fila = mysqli_fetch_assoc($resultado);
+                if(password_verify($contra, $fila['contrasenia'])){
+                    SESSION_START();
+                    $_SESSION['usuario']=$usuario;
+                    $_SESSION['trabajador']=$fila['tipo'];
+                    if($fila['tipo'] == "Promotor"){
+                        header("location:listadoProspectos.php");
+                    }else{
+                        header("location:listaProspectosEvaluar.php");
+                    }
+                } else{
+                    ?>
+                    <?php
+                    include("iniciarsesion.html");
+                    ?>
+                    <script type = "text/javascript"> 
+                        alert("Verifique que el usuario y contraseña sean correctos ");
+                    </script>
+                    <?php
                 }
-            } else{
-                ?>
-                <?php
-                include("iniciarsesion.html");
-                ?>
-                <script type = "text/javascript"> 
-                    alert("Verifique que el usuario y contraseña sean correctos ");
-                </script>
-                <?php
+                mysqli_free_result($resultado);
+                mysqli_close($con);
             }
-            mysqli_free_result($resultado);
-            mysqli_close($con);
+        }else{
+            header('location:iniciarsesion.html');
         }
     }else{
         header('location:iniciarsesion.html');
