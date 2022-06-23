7826a563830a7c95c6740ae26195d31c0b14c403
JasiveWong <jasivemw@gmail.com>
2022-06-23 10:03:56 -0600

    agregue espera de 3 segundos para cerrar sesion

--- cerrarsesion.php
+++ cerrarsesion.php
@@ -3,6 +3,7 @@
     if(isset($_SESSION['usuario'])&& isset($_SESSION['trabajador'])){
         SESSION_UNSET();
         SESSION_DESTROY();
+        sleep(3);
         header('location:index.html');
     }else{
         header('location:iniciarsesion.html');
